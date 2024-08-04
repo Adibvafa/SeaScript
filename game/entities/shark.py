ef5c5c03468c29757246ecf2fe284125b2d670b5
Grace Liu <graceliu.gll@gmail.com>
2024-08-04 01:37:07 -0400

    velocity decay

--- game/entities/shark.py
+++ game/entities/shark.py
@@ -7,10 +7,11 @@ from game.render import textures
 
 
 class Shark(Entity):
-    ticks_till_move: int = 50 + rand.randint(0, 50)
+    ticks_till_move: int = 200 + rand.randint(0, 50)
 
     def __init__(self, pos: tuple[float, float]):
         super().__init__(pos, (5, 3))
+        self.velocity_decay = 0.999
 
     def type(self) -> EntityType:
         return EntityType.SHARK
@@ -18,11 +19,11 @@ class Shark(Entity):
     def tick(self):
         self.ticks_till_move -= 1
         if self.ticks_till_move == 0:
-            self.ticks_till_move = 75 + rand.randint(0, 50)
+            self.ticks_till_move = 200 + rand.randint(0, 50)
             if rand.random() < 0.4:
-                self.velocity = (0.3, 0.0)
+                self.velocity = (0.15, 0.0)
             elif 0.4 < rand.random() < 0.8:
-                self.velocity = (-0.3, 0.0)
+                self.velocity = (-0.15, 0.0)
             else:
                 self.velocity = (0.0, 0.02) if rand.random() < 0.5 else (0., -0.02)
         super().tick()
